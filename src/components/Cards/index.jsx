f1efe90c03272461d715f9a2ebb28953deb0e3bc
strickerkarin <strickerkarin@hotmail.com>
2021-10-22 14:03:47 -0300

    [ADD] issue 4 template bloque listado, estático sin responsive

---
+++ src/components/Cards/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+import Card from './Card.jsx';
+import data from './data.json';
+
+function Cards() {
+
+    return (
+        <div className="cardsContainer">
+            <div className="cardsBlock">
+                <h2>Recomendaciones</h2>
+                {data.map(e => <Card img={e.img} category={e.category} title={e.title} location={e.location} description={e.description} />)}
+            </div>            
+        </div>
+    );
+}
+
+export default Cards;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Categories from "../Categories/index.jsx";
 import Search from "../SearchBlock/Search.jsx";
+import Cards from "../Cards/index.jsx";
 
 export default function Home() {
   return (
     <>
       <Search />
       <Categories />
+      <Cards />
     </>
   );
 }
