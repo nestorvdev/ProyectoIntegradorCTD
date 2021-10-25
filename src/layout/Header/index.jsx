3a5b63d405ea8f0aa1b110f14c9a25ffaaa057a5
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-10-25 19:24:18 -0300

    [ADD] agregue opacidad cuando se abre el menu burger

--- src/layout/Header/index.jsx
+++ src/layout/Header/index.jsx
@@ -3,77 +3,78 @@ import { Link } from "react-router-3";
 import ValidCredentials from '../../credentials/ValidCredentials';
 import logo from "./img/logo 1DB.png";
 import './style.css';
+import "../style.css"
 import MenuBurger from '../../components/MenuBurger/MenuBurger';
 import { HeaderWrapper } from '../../components/MenuBurger/HeaderStyles';
 import MenuButton from '../../components/MenuBurger/MenuButton';
 
-export default function Header(props) {
+export default function Header({ isLogged, showBurger, setShowBurger }) {
 
-    const [activeCreate, setIsActiveCreate] = useState((window.location.pathname==="/create")?true:false)
-    const [activeLogin, setIsActiveLogin] = useState((window.location.pathname==="/login")?true:false)
-    
+    const [activeCreate, setIsActiveCreate] = useState((window.location.pathname === "/create") ? true : false)
+    const [activeLogin, setIsActiveLogin] = useState((window.location.pathname === "/login") ? true : false)
+
     const ruta = window.location.pathname;
 
     function hide() {
-        if (ruta==="/login") {
+        if (ruta === "/login") {
             setIsActiveCreate(false)
             setIsActiveLogin(true)
-        } else if (ruta==="/create") {
+        } else if (ruta === "/create") {
             setIsActiveLogin(false)
             setIsActiveCreate(true)
-        } else if(ruta==="/"){
+        } else if (ruta === "/") {
             setIsActiveLogin(false)
             setIsActiveCreate(false)
         }
     };
-    
+
     useEffect(
-        ()=> hide()
+        () => hide()
     )
-    
-    const showUserName = (props.isLogged) ? "user loggedIn" : "user"; 
-    const hideButtons = (props.isLogged) ? "buttons user" : "buttons";
+
+    const showUserName = (isLogged) ? "user loggedIn" : "user";
+    const hideButtons = (isLogged) ? "buttons user" : "buttons";
     const iniciales = `${ValidCredentials.nombre.slice(0, 1).toUpperCase()}${ValidCredentials.apellido.slice(0, 1).toUpperCase()}`
 
-    
+
 
     function handleLogOut() {
         sessionStorage.setItem("log", "false")
     }
-    
-    const [showBurger, setShowBurger] = useState(false)
 
-    function handleShow(){
+    function handleShow() {
         setShowBurger(true)
     }
-    function handleHide(){
+    function handleHide() {
         setShowBurger(false)
     }
 
     return (
-        <HeaderWrapper className="header">
-            <Link to="/" className="home" onClick={handleHide}>
-                <div className="logo">
-                    <img src={logo} alt="logo" />
-                    <h3>Sentite como en tu hogar</h3>
+        <HeaderWrapper className="header" >
+            <div className={showBurger === true ? "headerTop opacity" : "headerTop"}>
+                <Link to="/" className="home" onClick={handleHide}>
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                        <h3>Sentite como en tu hogar</h3>
+                    </div>
+                </Link>
+                <div className={hideButtons}>
+                    <Link to="/create"><button className={activeCreate ? "hideButton" : null} >Crear Cuenta</button></Link>
+                    <Link to="/login"><button className={activeLogin ? "hideButton" : null} >Iniciar Sesion</button></Link>
                 </div>
-            </Link>
-            <div className={hideButtons}>
-                <Link to="/create"><button className={activeCreate ? "hideButton" : null} >Crear Cuenta</button></Link>
-                <Link to="/login"><button className={activeLogin ? "hideButton" : null} >Iniciar Sesion</button></Link>
+                <div className={showUserName}>
+                    <div className="logoName">
+                        <p>{iniciales}</p>
+                    </div>
+                    <div className="text">
+                        <h3 className="great">Hola,</h3>
+                        <h3 className="name">{ValidCredentials.nombre} {ValidCredentials.apellido}</h3>
+                    </div>
+                    <div className="close"><a href="/" onClick={handleLogOut}>X</a> </div>
+                </div>
+                <MenuButton show={showBurger} handleShow={handleShow} />
             </div>
-            <div className={showUserName}>
-                <div className="logoName">
-                    <p>{iniciales}</p>
-                </div>
-                <div className="text">
-                    <h3 className="great">Hola,</h3>
-                    <h3 className="name">{ValidCredentials.nombre} {ValidCredentials.apellido}</h3>
-                </div>
-                <div className="close"><a href="/" onClick={handleLogOut}>X</a> </div>
-            </div>
-            <MenuBurger show={showBurger} handleShow={handleShow} handleHide={handleHide} isLogged={props.isLogged} iniciales={iniciales} activeLogin={activeLogin} activeCreate ={activeCreate} handleLogOut={handleLogOut}/>
-            <MenuButton show={showBurger} handleShow={handleShow} /> 
+            <MenuBurger show={showBurger} handleHide={handleHide} isLogged={isLogged} iniciales={iniciales} activeLogin={activeLogin} activeCreate={activeCreate} handleLogOut={handleLogOut} />
         </HeaderWrapper>
     )
 }
--- src/layout/LayoutPrincipal.js
+++ src/layout/LayoutPrincipal.js
@@ -1,16 +1,23 @@
-import Header from "./Header"
-import Footer from "./Footer"
-import { BrowserRouter } from "react-router-dom"
+import "./style.css";
+import { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter } from "react-router-dom";
+
 
 export default function LayoutPrincipal(props) {
+    const [showBurger, setShowBurger] = useState(false)
+    console.log(showBurger);
 
     return (
-        <BrowserRouter>
-            <Header isLogged={props.route.isLogged} hide={props.route.hide}/>
-            <main>
-                {props.children}
-            </main>
-            <Footer />
-        </BrowserRouter>
+        <div className={showBurger === true ? "opacity" : null}>
+            <BrowserRouter >
+                <Header isLogged={props.route.isLogged} showBurger={showBurger} setShowBurger={setShowBurger} />
+                <main>
+                    {props.children}
+                </main>
+                <Footer />
+            </BrowserRouter>
+        </div>
     )
 }
