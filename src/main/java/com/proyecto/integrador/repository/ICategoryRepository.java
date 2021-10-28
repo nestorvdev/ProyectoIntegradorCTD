91cbe5162f9d6aebe3e976be00764a28f81e0e8b
tatianarincon96 <tatianalunarincon@gmail.com>
2021-10-27 23:08:49 -0300

    [DELETE] archivos repetidos

--- src/main/java/com/proyecto/integrador/repository/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.integrador.repository;
-
-import com.proyecto.integrador.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICategoryRepository extends JpaRepository<Category, Integer> {
-}
