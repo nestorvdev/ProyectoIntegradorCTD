d81415999ecc158b28eafa8dbfb90e27a3143e13
tatianarincon96 <tatianalunarincon@gmail.com>
2021-11-02 11:39:50 -0300

    [ADD] conexión con api de categorías Back

--- frontend/src/components/Categories/index.jsx
+++ frontend/src/components/Categories/index.jsx
@@ -2,22 +2,43 @@ import React from 'react';
 import Styles from './styles.module.css';
 import StylesApp from "../../App.module.css"
 import Category from './Category'
+import axios from "axios";
+import {useState , useEffect } from 'react';
 
-function Categories() {    
+function Categories() { 
+    const baseURL = "http://localhost:8080/categories/all";
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+    axios
+      .get(baseURL)
+      .then((response) => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setErrorMessage(error);
+      });
+    },[]);
+
+    if (errorMessage) return <p>{errorMessage}</p>;
     return (
         <>
-            <div className={`${Styles.categoryContainer} ${StylesApp.delimiter}`}>
-                <div className={`${Styles.categoryBlock} ${StylesApp.delimiterChild}`}>
+            <div className="categoryContainer delimiter">
+                <div className="categoryBlock delimiterChild">
                     <h2>Buscar por tipo de alojamiento</h2>
-                    <div className={Styles.categoryBox}>
-                        <Category title="Hoteles" image={Styles.image1} />
-                        <Category title="Hostels" image={Styles.image2} />
-                        <Category title="Departamentos" image={Styles.image3} />
-                        <Category title="Bed and Breakfast" image={Styles.image4} />                        
-                    </div>
+                    { loading ? ( <p>Loading Data...</p>
+                    ) : ( <div className="categoryBox">
+                        {data.map((category, index) => (
+                                      <Category key={index} title={category.title} image="image1" />
+                                    ))}
+                        </div>
+                    )}
                 </div>
-            </div> 
-        </>        
+            </div>
+        </>
     );
 }
 
