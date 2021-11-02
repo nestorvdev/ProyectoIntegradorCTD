a05a7953a44e8b7ce92f72b69f131e2897df322a
strickerkarin <strickerkarin@hotmail.com>
2021-11-02 13:26:07 -0300

    Agregamos estilos y modificamos toma de imagenes

--- frontend/src/components/Categories/Category.jsx
+++ frontend/src/components/Categories/Category.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Styles from './styles.module.css';
 
 function Category(props) {
-    const { image, title } = props;
+    const { imageUrl, title, description } = props;
     return (       
         <div className={Styles.categoryCard}>
-            <div className= {`${Styles.categoryImage} ${image}`}></div>
+            <img className= {Styles.categoryImage} src={imageUrl} alt="imagen 1" />
             <div className={Styles.categoryType}>
                 <h3>{title}</h3>
-                <p>807.105 hoteles</p>
+                <p>{description}</p>
             </div>
         </div >        
     )                        
--- frontend/src/components/Categories/index.jsx
+++ frontend/src/components/Categories/index.jsx
@@ -3,15 +3,15 @@ import Styles from './styles.module.css';
 import StylesApp from "../../App.module.css"
 import Category from './Category'
 import axios from "axios";
-import {useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
-function Categories() { 
-    const baseURL = "http://localhost:8080/categories/all";
-    const [data, setData] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [errorMessage, setErrorMessage] = useState("");
+function Categories() {
+  const baseURL = "http://localhost:8080/categories/all";
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
 
-    useEffect(() => {
+  useEffect(() => {
     axios
       .get(baseURL)
       .then((response) => {
@@ -21,25 +21,26 @@ function Categories() {
       .catch((error) => {
         setErrorMessage(error);
       });
-    },[]);
+  }, []);
 
-    if (errorMessage) return <p>{errorMessage}</p>;
-    return (
-        <>
-            <div className="categoryContainer delimiter">
-                <div className="categoryBlock delimiterChild">
-                    <h2>Buscar por tipo de alojamiento</h2>
-                    { loading ? ( <p>Loading Data...</p>
-                    ) : ( <div className="categoryBox">
-                        {data.map((category, index) => (
-                                      <Category key={index} title={category.title} image="image1" />
-                                    ))}
-                        </div>
-                    )}
-                </div>
+  if (errorMessage) return <p>{errorMessage}</p>;
+  return (
+    <>
+        <div className={`${Styles.categoryContainer} ${Styles.delimiter}`}>
+            <div className={`${Styles.categoryBlock} ${Styles.delimiterChild}`}>
+                <h2>Buscar por tipo de alojamiento</h2>
+                { loading ? ( <p>Loading Data...</p>
+                ) : ( <div className={Styles.categoryBox}>
+                    {data.map((category, index) => (
+                                  <Category key={index} title={category.title} imageUrl={category.url} description={category.description} />
+                                ))}
+                    </div>
+                )}
             </div>
-        </>
-    );
+        </div>
+    </>
+);
+
 }
 
 export default Categories;
