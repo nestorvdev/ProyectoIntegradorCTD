24fffc7da14dae420597f79ad54aa40ba7c648e9
Nestor Leandro Villalobos <leandrovillaloboscarrillo@gmail.com>
2021-11-03 16:27:08 -0500

    [FIX] se carga el componente product

--- frontend/src/components/Product/product.jsx
+++ frontend/src/components/Product/product.jsx
@@ -1,21 +1,61 @@
-import React from "react";
-import {useParams} from "react-router-dom";
-import data from "../Cards/data.json";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import TopBar from "./topBar";
 import Styles from './styles.module.css';
 
 
-function Product() {
-    let {id} = useParams();
+function Product(props) {
+    let { id } = useParams();
+
+    /* se crea el objeto vacio */
+    
+
+    let productAux = {
+        nombre: "nombreHotel",
+        descripcion: "descripcion hotel",
+        categoria: {
+            title: "tituloCategoria",
+            description: "descripcionCategoria",
+            imageUrl: "imagenCategoria"
+        },
+        imagenes: [
+            { id: 1, titulo: "fotoImagen1", url: "fotoImagen1" },
+            { id: 2, titulo: "fotoImagen2", url: "fotoImagen2" },
+            { id: 3, titulo: "fotoImagen2", url: "fotoImagen2" },
+            { id: 4, titulo: "fotoImagen2", url: "fotoImagen2" },
+            { id: 5, titulo: "fotoImagen2", url: "fotoImagen2" },
+            { id: 6, titulo: "fotoImagen2", url: "fotoImagen2" },
+            { id: 7, titulo: "fotoImagen2", url: "fotoImagen2" },
+            { id: 8, titulo: "fotoImagen2", url: "fotoImagen2" },
+        ],
+        caracteristicas: [
+            { id: 1, titulo: "wifi", icon: "fas fa-wifi", estado: true },
+            { id: 2, titulo: "pool", icon: "fas fa-swimmer", estado: true }
+        ],
+        latitud: -12.1234,
+        longitud: 12.1234,
+        calificacion: 9.5,
+        favorito: true,
+        ciudad: "ciudad",
+        pais: "pais",
+    }
+
+    const [product, setProduct] = useState(productAux);
+
+    /* <h1>Producto: {id}</h1>            
+                <h2>Soy un increíble alojamiento</h2>
+                <p>{data[id].title}</p>
+                <p>{data[id].category}</p>
+                <p>{data[id].location}</p>
+                <img className={Styles.productImg} src={data[id].img}/>
+                <p>{data[id].description}</p> */
+
+
+    console.log(props,"product");
     return (
-        <div>
-            <h1>Producto: {id}</h1>            
-            <h2>Soy un increíble alojamiento</h2>
-            <p>{data[id].title}</p>
-            <p>{data[id].category}</p>
-            <p>{data[id].location}</p>
-            <img className={Styles.productImg} src={data[id].img}/>
-            <p>{data[id].description}</p>
-        </div>
+        <section>
+           <TopBar categoria={product.categoria.title} nombre={product.nombre} goBack={props.history.goBack}/>
+        </section>
     );
 }
 
