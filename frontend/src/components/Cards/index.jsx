bc28ae3ea4f7908ef4b6e5a9b784fa3cc4960c22
strickerkarin <strickerkarin@hotmail.com>
2021-11-03 19:03:06 -0300

    Arreglo Link de botón y hago toggle de heart

--- frontend/src/components/Cards/index.jsx
+++ frontend/src/components/Cards/index.jsx
@@ -3,7 +3,6 @@ import Styles from './styles.module.css';
 import StylesApp from "../../App.module.css"
 import Card from './Card.jsx';
 import data from './data.json';
-import { Link } from "react-router-dom";
 
 function Cards() {
 
@@ -12,10 +11,8 @@ function Cards() {
             <div className={`${Styles.cardsBlock} ${StylesApp.delimiterChild}`}>
                 <h2>Recomendaciones</h2>
                 <div className={Styles.cardsBox}>
-                    {data.map((e, index) =>
-                    <Link to={`/product/${index}`} key={index}> 
+                    {data.map((e, index) =>                    
                         <Card img={e.img} category={e.category} title={e.title} location={e.location} description={e.description} key={index}/>
-                    </Link>
                     )}
                 </div>
             </div>
