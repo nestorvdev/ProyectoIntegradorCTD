31404d19f7c43d3510416ec91ddfb2f3928bf4ad
Nestor Leandro Villalobos <leandrovillaloboscarrillo@gmail.com>
2021-11-06 09:15:05 -0500

    [FIX] carga completa del modulo map y responsive

--- frontend/src/components/Product/Map.jsx
+++ frontend/src/components/Product/Map.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { Marker } from '@react-google-maps/api';
 
 function Map(props) {
     const {latitude, longitude, zoom } = props;
@@ -7,10 +8,12 @@ function Map(props) {
     const containerStyle = {width: '100%',height: '400px'};
     console.log(props,"map");
     return (
-    <LoadScript googleMapsApiKey="AIzaSyDoTLld8oN8wZm0avj07nwEv0FXqdfiaOE">
+    <LoadScript googleMapsApiKey="AIzaSyDoTLld8oN8wZm0avj07nwEv0FXqdfiaOE" loadingElement={<p>Cargando</p>}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={zoom} >
         { /* Child components, such as markers, info windows, etc. */ }
-        <></>
+        <>
+            <Marker position={{lat:latitude, lng:longitude}} />
+        </>
       </GoogleMap>
     </LoadScript>
   )
