8999a6d3d8470a2df66e8a003675b47d2814fa60
Mariel Romeo <marielceciliaromeo@hotmail.com>
2021-11-09 12:07:27 -0300

    [ADD] modal map en las cards + testing del componente

--- frontend/src/components/Product/Map.jsx
+++ frontend/src/components/Product/Map.jsx
@@ -6,7 +6,7 @@ function Map(props) {
     const {latitude, longitude, zoom } = props;
     const center = {lat:latitude, lng:longitude};
     const containerStyle = {width: '100%',height: '400px'};
-    console.log(props,"map");
+    
     return (
     <LoadScript googleMapsApiKey="AIzaSyDoTLld8oN8wZm0avj07nwEv0FXqdfiaOE" loadingElement={<p>Cargando</p>}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={zoom} >
