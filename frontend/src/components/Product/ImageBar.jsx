375a17dbe1b8aa34119a6b709e6d85bb9a60e9d3
strickerkarin <strickerkarin@hotmail.com>
2021-11-09 12:16:27 -0300

    arreglo estilos y like del Product

--- frontend/src/components/Product/ImageBar.jsx
+++ frontend/src/components/Product/ImageBar.jsx
@@ -7,38 +7,43 @@ import React, { useState } from "react";
 function ImageBar(props) {
     const [countSlider, setCountSlider] = useState(0); // se separa las imagenes a mostrar para la galeria de vista tablet y mobile
     const { images } = props;
-    console.log(props,"imageBar");
-    const changeSlider= ()=> countSlider === images.length-1?setCountSlider(0):setCountSlider(countSlider+1);   
+    console.log(props, "imageBar");
+    const changeSlider = () => countSlider === images.length - 1 ? setCountSlider(0) : setCountSlider(countSlider + 1);
     /*setTimeout(changeSlider,3000);*/
-    const openLightBox = (() => {props.setViewerIsOpen(true);});
-    const openShareModal = (() => {props.setShareIsOpen(true)})
+    const openLightBox = (() => { props.setViewerIsOpen(true); });
+    const openShareModal = (() => { props.setShareIsOpen(true) })
+    const [isLike, setLike] = useState("false");
 
-return (
-    <div className={`${Styles.imageBar} ${StylesApp.delimiter}`}>
-        <div className={`${Styles.imageBarChild} ${StylesApp.delimiterChild}`}>
-            <div className={Styles.barraSup}>
-                <img src={iconSocial} alt="iconSocial" className={Styles.iconImage} onClick={openShareModal}/>
-                <img src={iconHeart} alt="iconHeart" className={Styles.iconImage} />
-            </div>
-            <div className={Styles.barraInf}>
-                <div className={Styles.barraIzq}>
-                    <img src={images[0].url} alt={images[0].title} />
+    const handleToggle = (e) => {
+        setLike(!isLike);
+    }
+
+    return (
+        <div className={`${Styles.imageBar} ${StylesApp.delimiter}`}>
+            <div className={`${Styles.imageBarChild} ${StylesApp.delimiterChild}`}>
+                <div className={Styles.barraSup}>
+                    <img src={iconSocial} alt="iconSocial" className={Styles.iconImage} onClick={openShareModal} />
+                    <svg className={Styles.iconHeart} onClick={handleToggle} xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 25 25"><path className={isLike ? Styles.heartColor : Styles.heartColor2} d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /></svg>
                 </div>
-                <div className={Styles.barraDer}>
-                    <img src={images[0].url} alt={images[0].title} />
-                    <img src={images[0].url} alt={images[0].title} />
-                    <img src={images[0].url} alt={images[0].title} />
-                    <img src={images[0].url} alt={images[0].title} />
-                   <div className={Styles.verMas} onClick={openLightBox}>Ver Más</div>
-                </div>
-                <div className={Styles.slider}  onClick={changeSlider} >
-                    <img src={images[countSlider].url} alt={images[countSlider].title}/>
-                    <p>{(countSlider+1) +"/" +(images.length)}</p> 
+                <div className={Styles.barraInf}>
+                    <div className={Styles.barraIzq}>
+                        <img src={images[0].url} alt={images[0].title} />
+                    </div>
+                    <div className={Styles.barraDer}>
+                        <img src={images[0].url} alt={images[0].title} />
+                        <img src={images[0].url} alt={images[0].title} />
+                        <img src={images[0].url} alt={images[0].title} />
+                        <img src={images[0].url} alt={images[0].title} />
+                        <div className={Styles.verMas} onClick={openLightBox}>Ver Más</div>
+                    </div>
+                    <div className={Styles.slider} onClick={changeSlider} >
+                        <img src={images[countSlider].url} alt={images[countSlider].title} />
+                        <p>{(countSlider + 1) + "/" + (images.length)}</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default ImageBar;
--- frontend/src/components/Product/ScoreBar.jsx
+++ frontend/src/components/Product/ScoreBar.jsx
@@ -23,7 +23,7 @@ function ScoreBar(props) {
         <div className={`${Styles.scoreBar} ${StylesApp.delimiter}`}>
             <div className={`${Styles.scoreBarChild} ${StylesApp.delimiterChild}`}>
                 <div className={Styles.barraIzq}>
-                    <p> <img src={iconLocation} alt="iconLocation" className={Styles.iconLocation}/>  {" " + props.city.name + " , " + props.city.country}</p>
+                    <p> <img src={iconLocation} alt="iconLocation" className={Styles.iconLocation}/>  {" " + props.city.name + ", " + props.city.country}</p>
                     <p className={Styles.scoreBarReference}>{props.reference}</p>
                 </div>
                 <div className={Styles.barraDer}>
