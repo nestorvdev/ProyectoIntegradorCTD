8d241bbf49ac76d128aabffcd6cae389e3a3c58e
strickerkarin <strickerkarin@hotmail.com>
2021-11-10 13:36:22 -0300

    [FIX] Sólo se puede likear si está logueado

--- frontend/src/components/Cards/Card.jsx
+++ frontend/src/components/Cards/Card.jsx
@@ -46,11 +46,13 @@ function Card({ image, cardCategory, name, city, country, description, id, refer
         setMapIsOpen(false);
     };
 
+    let loggued = sessionStorage.getItem("log");
+
     return (
         <div className={Styles.cardBox}>
 
             <div className={Styles.cardImage}>
-                <svg className={Styles.iconHeart} onClick={handleToggle} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path className={isLike ? Styles.heartColor : Styles.heartColor2} id="heart" d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /></svg>
+                <svg className={Styles.iconHeart} onClick={loggued === "true"? handleToggle : null} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path className={isLike ? Styles.heartColor : Styles.heartColor2} id="heart" d="M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z" /></svg>
                 <img className={Styles.image} src={image} alt="" />
             </div>
 
