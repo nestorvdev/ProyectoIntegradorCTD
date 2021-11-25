2d2d8b7acf730412ca8c0c5646eaae3ebdb0888c
tatianarincon96 <tatianalunarincon@gmail.com>
2021-11-25 11:47:27 -0300

    [DELETE] carpetas back y front

--- backend/src/main/java/com/proyecto/integrador/exceptions/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.integrador.exceptions;
-
-public class BadRequestException extends Exception {
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
