4ed9f0054c6c35b3eed79c753132bf62bf6af190
Nestor Villalobos <nestor@Nestors-MacBook-Air.local>
2024-02-22 12:47:17 -0800

    [DEL]remove node_modules

--- node_modules/@bedrock-layout/use-forwarded-ref/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export default function useForwardedRef<T>(forwardedRef: React.Ref<T>): React.MutableRefObject<T>;
--- node_modules/@bedrock-layout/use-stateful-ref/lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-declare function useStatefulRef<T>(initialVal?: null): React.MutableRefObject<T>;
-export default useStatefulRef;
